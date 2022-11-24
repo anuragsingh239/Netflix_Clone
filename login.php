3dae1a722d8186cb66b56d9394b5b17da3f0b7ad
Anurag Singh <85439621+anuragsingh239@users.noreply.github.com>
2022-11-24 18:49:26 +0530

    Add files via upload

---
+++ login.php
@@ -0,0 +1,115 @@
+<?php 
+
+session_start();
+
+	include("connection.php");
+	include("functions.php");
+
+
+	if($_SERVER['REQUEST_METHOD'] == "POST")
+	{
+		//something was posted
+		$user_name = $_POST['user_name'];
+		$password = $_POST['password'];
+
+		if(!empty($user_name) && !empty($password) && !is_numeric($user_name))
+		{
+
+			//read from database
+			$query = "select * from users where user_name = '$user_name' limit 1";//to get only one result
+			$result = mysqli_query($con, $query);
+
+			if($result)
+			{
+				if($result && mysqli_num_rows($result) > 0)
+				{
+
+					$user_data = mysqli_fetch_assoc($result);
+					
+					if($user_data['password'] === $password)
+					{
+
+						$_SESSION['user_id'] = $user_data['user_id'];//user id validation
+						header("Location: index.php");
+						die;
+					}
+				}
+			}
+			
+			echo "<style type='text/css'>
+            h2 {
+                font-weight: 700;
+                color: green;
+                align:center;
+            }
+            .text {
+                font-weight: bold;
+            }
+        </style>
+        <h2>
+          Invalid user name or password
+        </h2>";
+		}else
+		{
+			echo "wrong username or password!";
+		}
+	}
+
+?>
+
+
+<!DOCTYPE html>
+<html>
+<style>
+body {
+background-image: url(login.jpg);
+background-color: black;
+}
+</style>
+</head>
+<body>
+	<style type="text/css">
+	
+	#text{
+
+		height: 25px;
+		border-radius: 5px;
+		padding: 4px;
+		border: solid thin #aaa;
+		width: 100%;
+	}
+
+	#button{
+
+		padding: 10px;
+		width: 100px;
+		color: white;
+		background-color: lightblue;
+		border: none;
+	}
+
+	#box{
+
+		background-color: grey;
+		margin: auto;
+		width: 300px;
+		padding: 20px;
+	}
+
+	</style>
+
+	<div id="box">
+		
+		<form method="post">
+			<div style="font-size: 20px;margin: 10px;color: white;">Login</div>
+
+			<input id="text" type="text" name="user_name"><br><br>
+			<input id="text" type="password" name="password"><br><br>
+
+			<input id="button" type="submit" value="Login"><br><br>
+
+			<a href="signup.php">Click to Signup</a><br><br>
+		</form>
+	</div>
+</body>
+</html>
